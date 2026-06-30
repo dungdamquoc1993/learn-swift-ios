7ce8510ce8463c7ec1dc2c9aebf7f6351ad8cc3a
dungdq <dungdq3@gmail.com>
2026-06-30 09:06:57 +0700

    Initial Commit

---
+++ smith/smithApp.swift
@@ -0,0 +1,17 @@
+//
+//  smithApp.swift
+//  smith
+//
+//  Created by DAM QUOC DUNG on 30/6/26.
+//
+
+import SwiftUI
+
+@main
+struct smithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
